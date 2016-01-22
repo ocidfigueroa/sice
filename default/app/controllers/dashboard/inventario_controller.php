260d4700388cfa159230e927e2cae8ced4bf9e90
Oscar Cid <ocid.figueroa@gmail.com>
2016-01-22 00:00:27 -0300

    Inventario
    inicio inventario

---
+++ default/app/controllers/dashboard/inventario_controller.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ *
+ * Descripcion: Controlador para el control de inventario
+ *
+ * @category    
+ * @package     Controllers 
+ */
+
+class InventarioController extends BackendController {
+    
+    public $page_title = 'Principal';
+    
+    public $page_module = 'Dashboard';
+    
+    public function index() { 
+        
+    }
+
+}
