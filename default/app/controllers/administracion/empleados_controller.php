47df9bab046514259a339993ac4a3d58cee0131a
Oscar Cid <ocid.figueroa@gmail.com>
2016-01-25 23:09:57 -0300

    Administración - Empleados
    se crea controlador de empleados

---
+++ default/app/controllers/administracion/empleados_controller.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ *
+ * Descripcion: Controlador para la mantencion de los empleados
+ *
+ * @category    
+ * @package     Controllers 
+ */
+
+class EmpleadosController extends BackendController {
+    
+    public $page_title = 'Principal';
+    
+    public $page_module = 'Dashboard';
+    
+    public function index() { 
+        
+    }
+
+}
